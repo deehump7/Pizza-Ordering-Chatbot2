ebd5a9847352b68f915fb314dfa92c73562178e3
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-02-20 08:19:44 -0800

    Added Data Validation

--- Pizza_Ordering_Chatbot.py
+++ Pizza_Ordering_Chatbot.py
@@ -1,17 +1,35 @@
 print("Hello, my name is Alex your virtual assitant. I will help you order a pizza! ")
 print("I am going to ask you a few questions. Please press enter after each answer.")
 userName = input("\nEnter your name:  ")
+while (len(userName) == 0):
+        userName = input("Oh No! Name cannot be blank please enter your name:  ")
+
 if userName.lower() == "dee":
     print("\nMy creator," + userName + ". Pleasure to serve you!")
 else:
    print("\nHello, "   +  userName + ". Nice to meet you!")
 
 size = input("\nWhat size do you want to order?  Enter small, medium, or large:  ")
+while size.lower() != "small" and size.lower() != "medium" and size.lower() != "large":
+    size = input("That is not a valid value. Please enter small, medium, or large:  ")
+while (len(size) == 0):
+        size = input("Oh No! Size cannot be blank please enter small, medium or large:  ")
 flavor = input("\nEnter the flavor of pizza:  ")
+while (len(flavor) == 0):
+        flavor = input("Oh No! Flavor cannot be blank please enter a flavor of pizza to enjoy! :  ")
 crustType = input("\nWhat type of crust would you like?:  ")
+while (len(crustType) == 0):
+        crustType = input("Oh No! Crust Type cannot be blank. Please enter a Crust Type,for example; garlic or stuffed?:  ")
 quantity = input("\nHow many pizzas would you like to order? Enter a numeric value:  ")
+while (len(quantity) == 0):
+        quantity = input("Oh No! Quantity cannot be blank. Please enter the number value of pizzas you would like to order.:  ")
+while not quantity.isdigit():
+    quantity = input("\nThis value is not recognized. Please enter a numeric value (i.e. 1234):  ")
 quantity = int(quantity)
 method = input("\nWill it be carry out or delivery?:  ")
+while method.lower() != "delivery" and method.lower() != "carry out" and method.lower != "carryout":
+  while (len(method) == 0):
+        method = input("Oh No! Method cannot be blank. Will it be carry-out or delivery?:  ")
 
 salesTax = 1.1
 
